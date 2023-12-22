528f00ea291f30b997a2ee9fb6c6977fb8cd78ee
Denimwearer <CorderoDixon12@yahoo.com>
2023-12-22 17:26:50 -0500

    second commit

--- __tests__/shuffle.test.js
+++ __tests__/shuffle.test.js
@@ -1,5 +1,29 @@
 const shuffle = require("../src/shuffle");
 
 describe("shuffle should...", () => {
-  // CODE HERE
+  test("return an array", () => {
+    const inputArray = [12, 21, 31, 14, 51];
+    const result = shuffle(inputArray);
+    expect(Array.isArray(result)).toBe(true);
+  });
+
+  test("return an array of the same length as the argument sent in", () => {
+    const inputArray = [21, 12, 31, 14, 51];
+    const result = shuffle(inputArray);
+    expect(result.length).toEqual(inputArray.length);
+  });
+
+  test("contains the same elements", () => {
+    const inputArray = [21, 12, 31, 14, 51];
+    const result = shuffle(inputArray);
+    inputArray.forEach((item) => {
+      expect(result).toContain(item);
+    });
+  });
+
+  test("items have been shuffled", () => {
+    const inputArray = [21, 12, 31, 14, 51];
+    const result = shuffle(inputArray);
+    expect(result).not.toEqual(inputArray);
+  });
 });
